ad8d20571833c1a101a026ecf6100bbdfe9160b6
nkyoko <notorious.droid@gmail.com>
2019-01-30 18:26:03 +0100

    basic movement works

--- Assets/scripts/game/movement.cs
+++ Assets/scripts/game/movement.cs
@@ -5,27 +5,49 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     public float speed = 1;
-
+    public GameObject ground;
+    private bool isgrounded = false;
     private Rigidbody2D rb;
 
-    void Start ()
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate ()
+    void FixedUpdate()
     {
-        float moveVertical = Input.GetAxis ("Vertical");
-
-        Vector2 movement = new Vector2 (moveVertical);
-
-        rb.AddForce (movement * speed);
-
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.D))
         {
-            rb.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
 
+        if (isgrounded == true)
+        {
+            if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.JoystickButton0))
+            {
+                rb.AddForce(new Vector2(0, 1), ForceMode2D.Impulse);
+            }
         }
     }
 
+    void OnCollisionEnter(Collision theCollision)
+    {
+        if (theCollision.gameObject.layer == 9)
+        {
+            isgrounded = true;
+        }
+    }
+
+    //consider when character is jumping .. it will exit collision.
+    void OnCollisionExit(Collision theCollision)
+    {
+        if (theCollision.gameObject.layer == 9)
+        {
+            isgrounded = false;
+        }
+    }
 }
