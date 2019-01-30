0c9944c9deb2746f916c71fb01f35c4e7939890b
nkyoko <notorious.droid@gmail.com>
2019-01-30 14:22:49 +0100

    collision works

---
+++ Assets/scripts/game/movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 1;
+
+    private Rigidbody2D rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate ()
+    {
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector2 movement = new Vector2 (moveVertical);
+
+        rb.AddForce (movement * speed);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+
+        }
+    }
+
+}
