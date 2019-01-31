e20881a48ef14c20c2cd31660c9dfac86a5a82b0
nkyoko <notorious.droid@gmail.com>
2019-01-31 13:31:56 +0100

    jumping works fine

--- Assets/scripts/game/movement.cs
+++ Assets/scripts/game/movement.cs
@@ -1,34 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements.StyleEnums;
 
 public class movement : MonoBehaviour
 {
     public float speed = 1;
-    public GameObject ground;
-    private bool isgrounded = false;
+    public Transform GroundCheck;
+    private bool isgrounded;
+    private bool m_FacingRight;
+    private float horizontalMove;
+    private bool jump;
     private Rigidbody2D rb;
+    private Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    
     }
 
     void FixedUpdate()
     {
+        isgrounded = Physics2D.Linecast(transform.position, GroundCheck.position,
+            1 << LayerMask.NameToLayer("Ground"));
+
+         
+        horizontalMove = Input.GetAxisRaw("Horizontal")*speed;
+        anim.SetFloat("Speed",Math.Abs(horizontalMove));
         if (Input.GetKey(KeyCode.D))
-        {
+        {   
+            
+            m_FacingRight = true;
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
             transform.Translate(Vector3.right * Time.deltaTime * speed);
+  
+
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * speed);
+            m_FacingRight = false;
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+         
+ 
         }
+        
+             
 
-        if (isgrounded == true)
-        {
+        if (isgrounded)
+        { 
+            jump = true;
             if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.JoystickButton0))
             {
+               
+                Debug.Log("Jump should be set to true");
                 rb.AddForce(new Vector2(0, 1), ForceMode2D.Impulse);
             }
         }
@@ -42,7 +70,6 @@ public class movement : MonoBehaviour
         }
     }
 
-    //consider when character is jumping .. it will exit collision.
     void OnCollisionExit(Collision theCollision)
     {
         if (theCollision.gameObject.layer == 9)
